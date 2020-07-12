f9284821c2426d10231ed43feec5cea01ea1f8fa
Justin <60296568+ComradeDiamond@users.noreply.github.com>
2020-07-12 15:49:18 -0400

    Added AI
    - AIs are now in the game
    - Added basic AI features like AI being able to play cards
    - Changed the player classes more so the AI has more features
    - Player class now binds backend with frontend (the pun here is that bind() was the added method)
    - Configured AI positions based on the number of AIs that exist
    
    To do:
    - Make AI playing cards animated, with directions based on the number of AIs

--- classes.js
+++ classes.js
@@ -1,3 +1,17 @@
+/* Justin Notation (Hmm maybe give it a fancy name: Justin Object Location Systsem)
+
+Look after a object declaration. 
+
+If it ends in JS, chances are, it's an important javascript object but doesn't reference anything in the frontend
+If it ends in CSS, the thing belongs to CSS
+If it ends in HTML, it is a direct HTML name or ID 
+If it ends in nothing but references an object, it's a reference to the object with the HTML tag attributes (Or a "JS ID Object")
+
+ie. AnonomyousKittenHTML is probably the ID for the div
+AnonomyousKittenCSS is probably the class 
+AnonomyousKitten is probably the reference to AnomonyousKittenHTML (think of it as a replacement for stuff like divOut)
+*/
+
 const cardsName = { //JSON to keep track of the cards
 	ace : 1,
 	two : 2,
@@ -34,6 +48,22 @@ const cardsValues = [ //Basically flipping cardsName
 	'a Jack',
 	'a Queen',
 	'a King'
+]
+const valueLoop = [
+	null,
+	"ace",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+	"ten",
+	"Jack",
+	"Queen",
+	"King"
 ]
 function xOfY(x, y) { // Looks for ___ of ___. ie. ace of spades
 	return `${cardsName.x}-${cardItem.y}`
@@ -99,34 +129,101 @@ function Player()
 {
 	this.hand = [];
 	this.isAi = true;
+	this.aiDisplay = null;
 }
 Player.prototype.addCard = function(card)
 {
 	this.hand.push(card);
 }
-Player.prototype.playCard = function() //Gets rid of the card from your hand
+Player.prototype.bind = function(jsIDObject) //JS ID Object --> See Justin Notation
+//Binds the AI player to a frontend object
 {
-	/*for (var i in cardArray)
+	if (this.isAi)
 	{
-		let cardPosition = this.hand.indexOf(cardArray[i]);
-		cardArray.splice(cardPosition, 1);
+		this.aiDisplay = jsIDObject;
 	}
-	*/
-	let indexArray = [];
-	//Push and ppop
-	for (let i in this.hand)
+}
+Player.prototype.matchArray = function() //AI finds the number of cards that have matching ranks
+{
+	let tempIndexArray = [];
+
+	for (var i=0; i<this.hand.length; i++)
 	{
-		if (this.hand[i].up) //Up = play
+		if (this.hand[i].value == currentValue)
 		{
-			let cardPosition = this.hand.indexOf(this.hand[i]);
-			indexArray.push(cardPosition);
+			tempIndexArray.push(i);
 		}
 	}
 
-	//Goes backwards when clearing so former cards in this.hand are unaffected
-	for (let i=(indexArray.length-1); i>=0; i--) 
+	return tempIndexArray;
+}
+Player.prototype.playCard = function() //Gets rid of the card from your hand
+{	
+	//Resets the last played cards
+	lastPlayedCards = [];
+
+	//Seperates player card playing from AI card playing
+	if (this.isAi)
 	{
-		this.hand.splice(indexArray[i], 1);
+		let indexArray = this.matchArray();
+		let doesRandomCardRepeat = true; //Check whether the card index has already been in list
+
+		//If there is >2 matches, play all the matches
+
+		//If there is one card, it will play a second card with a 10% chance
+		if (indexArray.length == 1)
+		{
+			if (Math.round(Math.random() * 10) == 0) //10% chance it will play 2 cards
+			{
+				while (doesRandomCardRepeat) //Picks a random card in the AI hand that does not repeat
+				{
+					let randomNumber = Math.round(Math.random() * this.hand.length);
+
+					if (indexArray.indexOf(randomNumber) == -1)
+					{
+						indexArray.push(randomNumber);
+						doesRandomCardRepeat = false;
+					}
+				}
+			}
+		}
+		else if (indexArray.length == 0)
+		{
+			indexArray.push(Math.round(Math.random() * this.hand.length));
+		}
+
+		indexArray.sort(function(a, b){return b - a});
+
+		for (let i in indexArray) //Plays/Deletes the card in the hand that corresponds to the index/number inside the indexArray
+		{
+			let tempPlayedCard = this.hand.splice(indexArray[i], 1);
+			centerPile.push(tempPlayedCard);
+			lastPlayedCards.push(tempPlayedCard);
+		}
+
+		return indexArray.length;
+	}
+	else
+	{
+		//Index array logs all the indexes to remove later
+		let indexArray = [];
+
+		for (let i in this.hand)
+		{
+			if (this.hand[i].up) //Up = play
+			{
+				let cardPosition = this.hand.indexOf(this.hand[i]);
+				indexArray.push(cardPosition);
+			}
+		}
+
+		//Goes backwards when clearing so former cards in this.hand are unaffected
+		for (let i=(indexArray.length-1); i>=0; i--) 
+		{
+			let tempPlayedCard = this.hand.splice(indexArray[i], 1);
+			centerPile.push(tempPlayedCard);
+			lastPlayedCards.push(tempPlayedCard);
+		}
 	}
 }
 Player.prototype.updateHand = async function(stache) //Makes sure all the card appears in the player's hand eventually
